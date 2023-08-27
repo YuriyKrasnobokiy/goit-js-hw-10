921d5666408cc50e62c64a4d17d29aeb5d467dd7
Yuriy Krasnobokiy <krasnobokiy.yuriy@gmail.com>
2023-08-27 14:00:14 +0300

    fix search

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 import Notiflix from 'notiflix';
-import SlimSelect from 'slim-select';
-import 'slim-select/dist/slimselect.css';
+// import SlimSelect from 'slim-select';
+// import 'slim-select/dist/slimselect.css';
 
 const select = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
@@ -20,10 +20,6 @@ function createOption(arrBreedId) {
 
 fetchBreeds()
   .then(data => (select.innerHTML = createOption(data)))
-  // new SlimSelect({
-  //   select: select,
-  //   data: arrBreedId,
-  // })
   .catch(onError);
 
 select.addEventListener('change', onSelect);
@@ -31,21 +27,22 @@ select.addEventListener('change', onSelect);
 function onSelect(event) {
   event.preventDefault();
   loader.classList.remove('visually-hidden');
+  catInfo.classList.add('visually-hidden');
 
   const breedId = event.currentTarget.value;
   fetchCatByBreed(breedId)
     .then(data => {
       const { url, breeds } = data[0];
-      console.log(data[0]);
+
       catInfo.innerHTML = `
       <div class="cat-card">
         <div class="cat-img">
           <img src="${url}" alt="cat" width="600" height="auto"/>
         </div>
         <div class="cat-text">
-        <h1 class="cat-title">${breeds[0].name}</h1>
-        <h2 class="cat-temper">${breeds[0].temperament}</h2>
-        <p class="cat-about">${breeds[0].description}</p>
+        <h1 class="cat-title">${data[0].breeds[0].name}</h1>
+        <h2 class="cat-temper">${data[0].breeds[0].temperament}</h2>
+        <p class="cat-about">${data[0].breeds[0].description}</p>
         </div>
       </div>`;
 
@@ -54,14 +51,14 @@ function onSelect(event) {
     })
     .catch(onError);
 
-  select.value = null;
+  // select.value = null;
 }
 
 function onError() {
   Notiflix.Notify.failure(
     'Oops! Something went wrong! Try reloading the page!',
     {
-      position: 'center-center',
+      position: 'center-top',
     }
   );
 
@@ -70,21 +67,3 @@ function onError() {
   catInfo.classList.add('visually-hidden');
   select.classList.add('visually-hidden');
 }
-
-// function onSelect(event) {
-//   event.preventDefault();
-//   loader.classList.remove('visually-hidden');
-//   catInfo.classList.remove('visually-hidden');
-//   const selectedOption = event.target.value;
-
-//   fetchBreeds()
-//     .then(data => {
-//       const selectedBreed = data.find(breed => breed.id === selectedOption);
-//       renderCatCard([selectedBreed]);
-
-//       loader.classList.add('visually-hidden');
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// }
